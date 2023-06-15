405c78ba7a5f6ca3ad44d3c7b46d02df59fb62d4
Clancastle <105987220+Clancastle@users.noreply.github.com>
2023-06-15 17:34:56 -0500

    figured how to add files

---
+++ calculpy.py
@@ -0,0 +1,34 @@
+import math
+
+print("""
+Welcome to our calculator web app. 
+Basic uses are -- +, -, *, /, and others. 
+To check available commands, type -commands.
+
+First release will be simple, next will take only one input jammed together. 
+
+Enjoy!!
+""")
+
+while True:
+    x = float(input('Input number >> '))
+    op = input('Input operator >> ')
+    y = float(input('Input number >> '))
+
+    def plus():
+        pass
+
+
+    def minus():
+        pass
+
+
+    def times():
+        pass
+
+
+    def divide():
+        pass
+
+
+
