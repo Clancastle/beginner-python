1a3fdb1efb3ffc93bf56579b86649649b7622c6d
Clancastle <dennisungureanu0@gmail.com>
2023-06-16 16:36:27 -0500

    start on complex calculator.
    idea is, to gather it in one input, then split the input based on spaces, and try to place, str, float on them.

---
+++ complexcalculator.py
@@ -0,0 +1,28 @@
+import math
+
+z = input('Write an equation. ')
+
+# def add():
+input_split = z.split(' ')
+for i in input_split:
+    print(i)
+
+print(input_split)
+
+
+
+# def minus():
+#
+#
+# def times():
+#
+#
+# def divide():
+#
+
+def exponent():
+    pass
+
+
+def solve():
+    pass
