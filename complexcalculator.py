3c811f20d129a1ed07d3d6917c8a3bb08ee01b71
Clancastle <dennisungureanu0@gmail.com>
2023-06-16 21:49:35 -0500

    update
    turn input into float and str,
    creates foundation for next functions of math

--- complexcalculator.py
+++ complexcalculator.py
@@ -1,11 +1,5 @@
-import math
 
-#z = input('Write an equation. ').lower() #
-operators = ['!', "@", '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', ':', '/','[', ']', '{', '}', '=']
-
-z = "50 + 50"
-#z = "I love thinking"
-
+#####THOUGHTS ___ HOW I GOT TO MAKE IT -------- #####
 # def transform(): #can split into len(z), and go like, for a loop, if putting str on it
 # not work, then put float
 # -> could work this -> dont let the input be more than 3 (0, 1, 2), and split, or partition
@@ -14,17 +8,29 @@ z = "50 + 50"
 # nope does not work.
 #idea 2, make a for loop. so evry time it runs, it splits z, and the split parts
 # are then assigned a variable, like x1, x2, x3. but ive yet to learn
-#nope, i just put isdigit() and it is fixed. 
+#nope, i just put isdigit() and it is fixed.
 
-input_split = z.split(' ')
+#also an idea to split string if operater is touching a number
+# import math
+#
+#
+z = input('Write an equation. ')
+operators = ['!', "@", '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', ':', '/','[', ']', '{', '}', '=']
+
+input_split = z.split()
+
 print(input_split)
-#def is_number(): find out how to return values before
 for i in input_split:
     if i.isdigit():
+        i = float(i)
         print(i)
-# print(z.rindex(operators))
-# print(input_split.isdigit())
+        #if a digit, turn it into a float. then call math function
 
+for i in input_split:
+    for operator in operators:
+        if i == operator:
 
-
-
+            print(operator)
+            print(True)
+def solve():
+    pass
