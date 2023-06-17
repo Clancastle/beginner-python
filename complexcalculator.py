74c0d4da2a7ae57eb83cd39ce717c99b3569c4ab
Clancastle <dennisungureanu0@gmail.com>
2023-06-16 23:37:27 -0500

    adds
    if, tells if input is number or operator,
    and adds each to a list in order
     plan on calling that list in a function
    so the order is correct.
    f.e. -> operators_list.find('(')
    to correctly assume order of operations.

--- complexcalculator.py
+++ complexcalculator.py
@@ -9,28 +9,47 @@
 #idea 2, make a for loop. so evry time it runs, it splits z, and the split parts
 # are then assigned a variable, like x1, x2, x3. but ive yet to learn
 #nope, i just put isdigit() and it is fixed.
+# if while loop is only under the for loop, it doesnt stop, ever.
+#more efficient to use one for loop, and have several for nests, inside that one
 
 #also an idea to split string if operater is touching a number
-# import math
-#
-#
-z = input('Write an equation. ')
-operators = ['!', "@", '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', ':', '/','[', ']', '{', '}', '=']
+import math
 
-input_split = z.split()
+ # class or one big function with lots of ifs?
 
-print(input_split)
-for i in input_split:
-    if i.isdigit():
-        i = float(i)
-        print(i)
-        #if a digit, turn it into a float. then call math function
-
-for i in input_split:
-    for operator in operators:
-        if i == operator:
-
-            print(operator)
-            print(True)
-def solve():
+class Solve(): #operators is used...
     pass
+
+
+while True:
+
+    numbers_list_in_order = []
+    operator_list_in_order = []
+    z = input('Write an equation. ')
+
+    operators = ['+', '-','*' ,'/' , '^', '!', "@", '#', '$', '%','&', '(', ')', ':','[', ']', '{', '}', '=']
+
+    input_split = z.split() # split to identify each unit. #next is probably ('87+87')
+#to identify and separate by operator, or space #after 90+90 triggers error, run modified
+    #code down. like z.split(operators). <-- make that work
+# print(input_split)
+
+    for i in input_split: #
+        for operator in operators:
+            for yes in range(1):
+                if operator == operators[0]:
+                    #Solve.add(self)
+
+        if i.isdigit():
+            i = float(i)
+            numbers_list_in_order.append(i)
+            print(numbers_list_in_order)
+            #Solve(operator_list_in_order, numbers_list_in_order)
+
+        for operator in operators: #
+            if i == operator:
+                operator_list_in_order.append(operator)
+                print(operator_list_in_order)
+
+                #i can put the solving here, but functions are more challenging to me and
+                # i want to try and see what i can do. plus It's more neat
