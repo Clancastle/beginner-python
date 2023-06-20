b0f4568155b910e5d9c5f043a8e34a8ddaa09ecd
Clancastle <dennisungureanu0@gmail.com>
2023-06-20 14:59:50 -0500

    realization
    i realize that all the code that ive done in py, all of it is long, like 100lines, and i cant think straight lol

--- hangman.py
+++ hangman.py
@@ -1,8 +1,10 @@
 import random
 from itertools import count
 
+
 guessed_word = False #i dont think this has much effect but ill keep it.
-lives = [1, 2, 3, 4]
+lives = [1, 2, 3, 4] #lives = 10, while lives != 0
+
 word_site = "https://www.mit.edu/~ecprice/wordlist.10000"
 word_file = "/usr/share/dict/words"
 words = open(word_file).read().splitlines()
@@ -18,78 +20,72 @@ difficulty = input("""Choose your difficulty.
 (Easy) -- Just say you are a coward.
 Medium(Mid) -- That's fine...
 (Hard) -- Oh, a daredevil!
-->""")#1-5, 6-7, 8+ respectivly (letter count)
+->""").lower()#1-5, 6-7, 8+ respectivly (letter count)
 
 #print(hard_words) so the lists work.
 
 def call(difficulti):
     if difficulti == 'easy':
-        return random.choice(easy_words)
+        return random.choice(easy_words).lower()
     elif difficulti == 'mid':
-        return random.choice(medium_words)
+        return random.choice(medium_words).lower()
     elif difficulti == 'hard':
-        return random.choice(hard_words)
+        return random.choice(hard_words).lower()
 
 
 def letterGuessed(letter):
     if word.find(letter) == -1:
         return "Guess Failed."
+
     else:
         for i in word:
+            if i in letter:
+                print(i,end="") #copied, do not know how this works
+            else:
+                print('_',end=''),
 
-            index_letter = word.index(letter)
-            blanks = ['_'] * len(word)
-            for b in blanks:
-                print(blanks)
-                blanks.remove(b) #then join it
-                pprint(blanks)
-                blanks.insert(b, letter)
-
-                # bob = blanks.insert(index_letter, letter)
-                # blanks.insert(index_letter, letter)
-                # print(blanks)
-                # print(blanks.insert(index_letter, letter))
-
-                # print(type(blanks))
-                # print(index_letter)
-                print(i)
-                return blanks
 while not guessed_word:
     for i in count(1):
         if difficulty.lower() == 'easy':
             word = call('easy')
             print(word)
+
             break
+
         elif difficulty.lower() == 'mid':
             word = call('mid')
             print(word)
+
             break
+
         elif difficulty.lower() == 'hard':
             word = call('hard')
             print(word)
+
             break
+
         elif difficulty != 'easy' or 'mid' or 'hard':
             print('Sorry, what was that?')
             difficulty = input("""
     (Easy) -- Medium(Mid)-- (Hard)
-    ->""")
+    ->""").lower()
     print('BTW, We do not have machine learning here, so sometimes you will get a word you dont know.')
     print('Happy Guessing!')
     print('-')
 
-    guesses = input('Input your guess (a letter/ guess the word) ->')
+    guesses = input('Input your guess (a letter/ guess the word) ->').lower()
     for life in lives:
         if guesses == word:
             guessed_word = True
             print('You Win!')
             print('Play again?')
-            y_n = input('(y) or (n)')
+            y_n = input('(y) or (n)').lower()
 
             if y_n == 'y':
                 guessed_word = False
                 difficulty = input("""
     (Easy) -- Medium(Mid)-- (Hard)
-    ->""")
+    ->""").lower()
             elif y_n == 'n':
                 print('Sorry to see you go.')
             else:
@@ -97,15 +93,14 @@ while not guessed_word:
                 print("Didn't understand that.")
                 difficulty = input("""
     (Easy) -- Medium(Mid)-- (Hard)
-    ->""")
+    ->""").lower()
                 break
             #break #
         remaining = 4 - life
         print(letterGuessed(guesses))
 
-        print(f'{remaining} remaining life.') #if guessed 1 letter correctly
+        #print(f'{remaining} remaining life.') #if guessed 1 letter correctly
         # print(f'{lives - life} remaining')
-        guesses = input('Input your guess..')
 
 
 
