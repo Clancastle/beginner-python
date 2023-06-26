57a6b5f806bd4b5ca5f9fc89ff8976d9759c9fea
Clancastle <dennisungureanu0@gmail.com>
2023-06-26 09:35:56 -0500

    rps small code

--- incomplete-projects/rps.py
+++ incomplete-projects/rps.py
@@ -1,12 +1,44 @@
 import random
-#rock paper scissors game computer
+import time
+# rock paper scissors game computer
 
-tuple = (1, 4, 8, 'list', 9, None,)
 
-for i in range(len(tuple)):
-    print(i)
+def play():
+    feedback = random.choice(["r", "p", "s"])
+    print(feedback)  # it is better to do .upper() then give user lower since it has a lower margin error
+    user = input('You are playing rock, paper, scissors. \nrock (r); paper(p); scissors(s)\n').lower()
+    # r, p, s
+    # r > s; s > p; p > r
+    if user == feedback:  # tie
+        time.sleep(2)
+        print(f'A tie occurred. {user} = {feedback}')
 
-tuple.__mul__(5)
+    elif (user == 'r' and feedback == 's') or (user == 's' and feedback == 'p') or (user == 'p' and feedback == 'r'):
+        time.sleep(.3)
+        print(f'Player wins. {user} > {feedback}')
+        time.sleep(1)
+    elif (feedback == 'r' and user == 's') or (feedback == 's' and user == 'p') or (feedback == 'p' and user == 'r'):
+        time.sleep(.3)
+        print(f'Computer was \'{feedback}\'. New game coming up..')
+        time.sleep(1)
+    else:
+        print('Sorry, this doesn\'t match the database. ')
+        play()
 
-set = set((True, True))
-print(set)
+
+def playagain():
+    e = input('(y/n) Play again?\n').lower()
+    if e == 'n':
+        return 'n'
+    elif e == 'y':
+        pass
+    else:
+        print('Sorry try again?')
+        playagain()
+
+
+while True:
+    play()
+    if playagain() == 'n':
+        break
+
