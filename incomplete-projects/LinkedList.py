bb3d2dbb1e989dcd6e56e06b6d8fe3a1861310d6
Clancastle <dennisungureanu0@gmail.com>
2023-06-29 14:46:33 -0500

    Update LinkedList.py

--- incomplete-projects/LinkedList.py
+++ incomplete-projects/LinkedList.py
@@ -1,58 +1,106 @@
-#creating a linked list class and creating needed functions
-#also creating a binary search
+#how to --
+# list of integers in decreasing order
+# pick out the query by looking at as little indexes as possible
 
-list = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,11,1,1,1,1,1,1,1,1,1,11,1,1,1,1,1,1]
-
-# print(Node()) this results in it printing a address where on the ram this is stored.
-
-class Node():
-    def __init__(self, value):
-        self.data = value
-        self.next = None
-
-class LinkedList():
-    def __init__(self, head):
-        self.head = head
-
-    def append(self):
-        pass
-    def traverse(self):
-        pass
-    def delete(self):
-        pass
-    def reverse(self):
-        pass
-
-order = [30, 25, 20, 15, 10, 5, 3, 0]
-#Binary Search
-dict = {
-    "list": [],
-    'query': None,
-    "output": None
-}
-
-#find a number in a decreasing sorted list
+# state problem clearly
+# come up with some  example input/outputs, try to cover all edge cases
+# come up with solution. state in plain english
+# implement solution, fix any bugs.
+# analyze solution/algorithm, overcome inefficiency
+# repeat 3-5
 
 
-#search for all edge cases and remove
+def binary_search(list, query):
+    low, high = 0, len(list) - 1
 
-def BinarySearch(list, query):
-    lo, hi = 0, len(list) - 1  # since counting starts at 0
-
-
-    while lo <= hi:
-        med = (hi + lo) // 2  # lo is 0, but this is if it gets reassigned later during running the program
-        midnum = list[med]
-        print(midnum)
-        if query == midnum:
-            return midnum
-        elif query < midnum:
-            lo = med + 1
-            return lo
-        else: # if not equal or less, it is more than
-            hi = med - 1
-            return hi
+    while low <= high: # we will change low and high values to narrow down query
+        middle = (low + high) // 2
+        middle_number = list[middle]
+        #print(middle)
+        #print(middle_number)
+        if middle_number == query: #it returns the index, but if multiple, it returns the one it sees first, not the first index of query
+            return middle
+        elif query < middle_number:
+            low = middle + 1
+        else:
+            high = middle - 1
+    return -1
 
 
+class Nod(object):
+    def __init__(self, d, n = None):
+        self.data = d
+        self.next_node = n
+    def get_next(self):
+        return self.next_node
+    def set_next(self, n):
+        self.next_node = n
+    def get_data(self):
+        return self.data
+    def set_data(self, d):
+        self.data = d
 
-print(BinarySearch(order, 30))
+class Linked(object):
+    def __init__(self, r = None):
+        self.root = r
+        self.size = 0
+    def get_size(self):
+        return self.size
+    def add(self, d):
+        new_node = Nod(d, self.root)
+        self.root = new_node
+        self.size += 1
+    def remove(self, d):
+        this_node = self.root
+        prev_node = None
+        while this_node:
+            if this_node.get_data() == d:
+                if prev_node:
+                    prev_node.set_next(this_node.get_data())
+                else:
+                    self.root = this_node
+                self.size -= 1
+                return True
+            else:
+                prev_node = this_node
+                this_node = this_node.get_next()
+        return False
+    def find(self, d):
+        this_node = self.root
+        while this_node:
+            if this_node.get_data() == d:
+                return d
+            else:
+                this_node = this_node.get_next()
+        return None
+
+mylist = Linked()
+
+mylist.add(3)
+mylist.add(9)
+mylist.add(10)
+print(mylist.find(90))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
