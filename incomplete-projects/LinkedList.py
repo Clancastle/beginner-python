fcefc74f8c76420a0fc7a51552e994a971b9ed47
Clancastle <dennisungureanu0@gmail.com>
2023-07-01 19:53:17 -0500

    Update LinkedList.py

--- incomplete-projects/LinkedList.py
+++ incomplete-projects/LinkedList.py
@@ -9,78 +9,26 @@
 # analyze solution/algorithm, overcome inefficiency
 # repeat 3-5
 
+class Node():
+    value = None
+    next_n = None
 
-def binary_search(list, query):
-    low, high = 0, len(list) - 1
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return f"Node: {self.value} " #%self.value
 
-    while low <= high: # we will change low and high values to narrow down query
-        middle = (low + high) // 2
-        middle_number = list[middle]
-        #print(middle)
-        #print(middle_number)
-        if middle_number == query: #it returns the index, but if multiple, it returns the one it sees first, not the first index of query
-            return middle
-        elif query < middle_number:
-            low = middle + 1
-        else:
-            high = middle - 1
-    return -1
+class LinkedList():
+    pass
 
 
-class Nod(object):
-    def __init__(self, d, n = None):
-        self.data = d
-        self.next_node = n
-    def get_next(self):
-        return self.next_node
-    def set_next(self, n):
-        self.next_node = n
-    def get_data(self):
-        return self.data
-    def set_data(self, d):
-        self.data = d
-
-class Linked(object):
-    def __init__(self, r = None):
-        self.root = r
-        self.size = 0
-    def get_size(self):
-        return self.size
-    def add(self, d):
-        new_node = Nod(d, self.root)
-        self.root = new_node
-        self.size += 1
-    def remove(self, d):
-        this_node = self.root
-        prev_node = None
-        while this_node:
-            if this_node.get_data() == d:
-                if prev_node:
-                    prev_node.set_next(this_node.get_data())
-                else:
-                    self.root = this_node
-                self.size -= 1
-                return True
-            else:
-                prev_node = this_node
-                this_node = this_node.get_next()
-        return False
-    def find(self, d):
-        this_node = self.root
-        while this_node:
-            if this_node.get_data() == d:
-                return d
-            else:
-                this_node = this_node.get_next()
-        return None
-
-mylist = Linked()
-
-mylist.add(3)
-mylist.add(9)
-mylist.add(10)
-print(mylist.find(1))
-
+n = Node(1)
+nn = Node(2)
+nnn = Node(3)
+nn.next_n = n
+nn.next_n.next_n = nnn
+print(nn.next_n)
+print(nn.next_n.next_n)
 
 
 
